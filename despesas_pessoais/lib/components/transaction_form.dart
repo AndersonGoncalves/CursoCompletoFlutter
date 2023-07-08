f18d1387df0e8638d40f919a1f3eab175cbddb3d
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-07-08 15:12:12 -0300

    Dividindo o App em widgets

---
+++ despesas_pessoais/lib/components/transaction_form.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller:
+                  titleController, //onChanged: (newTitle) => titleController.text = newTitle,
+              decoration: const InputDecoration(
+                labelText: 'Título',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+            TextField(
+              controller:
+                  valueController, //onChanged: (newValue) => valueController.text = newValue,
+              decoration: const InputDecoration(
+                labelText: 'Valor (R\$)',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: TextButton(
+                    child: const Text(
+                      'Nova transação',
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    onPressed: () {
+                      print(titleController.text);
+                      print(valueController.text);
+                    },
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
