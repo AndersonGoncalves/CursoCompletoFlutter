1c55a48b2be2e61d66bea232fb59d1ef544d161f
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-07-26 14:31:41 -0300

    O problema com listas e StatefulWidget

--- despesas_pessoais/lib/components/transaction_item.dart
+++ despesas_pessoais/lib/components/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:despesas_pessoais/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     super.key,
     required this.tr,
@@ -11,6 +13,28 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction tr;
   final void Function(String p1) onRemove;
+
+  @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  static const colors = [
+    Colors.red,
+    Colors.purple,
+    Colors.orange,
+    Colors.blue,
+    Colors.black,
+  ];
+
+  Color? _backgroundColor;
+
+  @override
+  void initState() {
+    super.initState();
+    int i = Random().nextInt(5);
+    _backgroundColor = colors.elementAt(i);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,12 +68,13 @@ class TransactionItem extends StatelessWidget {
         */
         leading: CircleAvatar(
           radius: 30,
-          backgroundColor: Theme.of(context).colorScheme.primary,
+          backgroundColor:
+              _backgroundColor, //Theme.of(context).colorScheme.primary,
           child: Padding(
             padding: const EdgeInsets.all(7),
             child: FittedBox(
               child: Text(
-                'R\$${tr.value}',
+                'R\$${widget.tr.value}',
                 style: const TextStyle(
                   color: Colors.white,
                 ),
@@ -58,15 +83,15 @@ class TransactionItem extends StatelessWidget {
           ),
         ),
         title: Text(
-          tr.title,
+          widget.tr.title,
           style: Theme.of(context).textTheme.bodyLarge,
         ),
         subtitle: Text(
-          DateFormat('dd MMM y').format(tr.date),
+          DateFormat('dd MMM y').format(widget.tr.date),
         ),
         trailing: MediaQuery.of(context).size.width > 480
             ? TextButton.icon(
-                onPressed: () => onRemove(tr.id),
+                onPressed: () => widget.onRemove(widget.tr.id),
                 icon: Icon(Icons.delete,
                     color: Theme.of(context).colorScheme.error),
                 label: Text(
@@ -77,7 +102,7 @@ class TransactionItem extends StatelessWidget {
             : IconButton(
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).colorScheme.error,
-                onPressed: () => onRemove(tr.id),
+                onPressed: () => widget.onRemove(widget.tr.id),
               ),
       ),
     );
