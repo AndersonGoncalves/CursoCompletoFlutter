3f804a7dbcbb82bf15bc65a7ab791d9361c4b86d
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-07-29 13:55:26 -0300

    Tema e estilo da aplicacao

--- refeicoes/lib/main.dart
+++ refeicoes/lib/main.dart
@@ -10,9 +10,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
+      //Inicio - Difinindo o thema da aplicação //ou theme: ThemeData(primarySwatch: Colors.purple),
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.pink,
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
+      //Fim - Difinindo o thema da aplicação
       home: const CategoriesScreen(),
     );
   }
