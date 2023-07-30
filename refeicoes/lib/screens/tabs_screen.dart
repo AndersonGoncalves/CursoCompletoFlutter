4b34725b878a3b69c2d21a3c547a026d97b5bf96
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-07-30 14:32:20 -0300

    Criacao de TabBar inferior

--- refeicoes/lib/screens/tabs_screen.dart
+++ refeicoes/lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
 
+/*Navegação dentro do AppBar na parte superior
 class TabsScreen extends StatelessWidget {
   const TabsScreen({Key? key}) : super(key: key);
 
@@ -32,6 +33,67 @@ class TabsScreen extends StatelessWidget {
             FavoriteScreen(),
           ],
         ),
+      ),
+    );
+  }
+}
+*/
+
+//Navegação fora do AppBar na parte inferior)
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Map<String, Object>> _screens = [
+    {'title': 'Lista de Categorias', 'screen': const CategoriesScreen()},
+    {'title': 'Meus Favoritos', 'screen': const FavoriteScreen()},
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _screens[_selectedScreenIndex]['title'] as String,
+        ),
+      ),
+      body: _screens[_selectedScreenIndex]['screen'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        currentIndex: _selectedScreenIndex,
+        //type: BottomNavigationBarType.fixed, //Padrão
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context)
+                .colorScheme
+                .primary, //Não precisa se type: BottomNavigationBarType.fixed, //Padrão
+            icon: const Icon(Icons.category),
+            label: 'Categorias',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context)
+                .colorScheme
+                .primary, //Não precisa se type: BottomNavigationBarType.fixed, //Padrão
+            icon: const Icon(Icons.star),
+            label: 'Favoritos',
+          ),
+        ],
       ),
     );
   }
