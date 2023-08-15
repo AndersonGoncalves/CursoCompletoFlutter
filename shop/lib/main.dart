c13fecc82d449c6af490b5a5368b6af7973f8977
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-08-15 17:23:17 -0300

    Refatoracao

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop/models/product_list.dart';
+import 'package:shop/providers/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -12,7 +12,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -26,7 +25,7 @@ class MyApp extends StatelessWidget {
             secondary: Colors.deepOrange,
           ),
         ),
-        home: ProductsOverviewPage(),
+        home: const ProductsOverviewPage(),
         debugShowCheckedModeBanner: false,
         routes: {AppRoutes.productDetail: (_) => const ProductDetailPage()},
       ),
