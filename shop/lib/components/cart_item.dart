ed139d96431126989a3b502544dc50dcb4d1d803
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-08-21 14:12:17 -0300

    Criando os itens do carrinho

--- shop/lib/components/cart_item.dart
+++ shop/lib/components/cart_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/cart_item.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -8,6 +9,31 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(cartItem.name);
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text('${cartItem.price}',
+                    style: const TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+          ),
+          title: Text(cartItem.name),
+          subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+          trailing: Text('${cartItem.quantity}x'),
+        ),
+      ),
+    );
   }
 }
