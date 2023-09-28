77e6b209aba0574e5591b4f37fe1c55faba1b683
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-09-28 14:16:58 -0300

    Exibindo os produtos

--- shop/lib/providers/product_list.dart
+++ shop/lib/providers/product_list.dart
@@ -1,16 +1,14 @@
 import 'dart:convert';
 import 'dart:math';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   final _url =
       'https://shop-cod3r-78cd3-default-rtdb.firebaseio.com/products.json';
-  final List<Product> _items = dummyProducts;
+  final List<Product> _items = [];
 
   List<Product> get items => [..._items];
   List<Product> get favoriteItems =>
@@ -22,8 +20,21 @@ class ProductList with ChangeNotifier {
   }
 
   Future<void> loadProducts() async {
+    _items.clear();
     final response = await http.get(Uri.parse(_url));
-    print(jsonDecode(response.body));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((productId, productData) {
+      _items.add(Product(
+        id: productId,
+        name: productData['name'],
+        description: productData['description'],
+        price: productData['price'],
+        imageUrl: productData['imageUrl'],
+        isFavorite: productData['isFavorite'],
+      ));
+    });
+    notifyListeners(); //Notificando aos interessados que ouve uma mudança na lista
   }
 
   Future<void> saveProductFromData(Map<String, Object> data) {
