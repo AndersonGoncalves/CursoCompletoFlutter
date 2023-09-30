afa349ceb31f3aca543052219b1d8290203e853f
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-09-30 14:43:11 -0300

    Armazenando o pedido no firebase

--- shop/lib/utils/constants.dart
+++ shop/lib/utils/constants.dart
@@ -1,4 +1,7 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-cod3r-78cd3-default-rtdb.firebaseio.com/products';
+
+  static const orderBaseUrl =
+      'https://shop-cod3r-78cd3-default-rtdb.firebaseio.com/orders';
 }
