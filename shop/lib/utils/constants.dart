f2d0f84024a84db75955ae79dd48620a5a39fb2f
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-10-01 17:53:43 -0300

    Correcao na api key

--- shop/lib/utils/constants.dart
+++ shop/lib/utils/constants.dart
@@ -5,5 +5,5 @@ class Constants {
   static const orderBaseUrl =
       'https://shop-cod3r-78cd3-default-rtdb.firebaseio.com/orders';
 
-  static const webApiKey = 'AIzaSyAOB9uer_xxRprUZnoZSqeK3oenNmingOc';
+  static const webApiKey = 'AIzaSyCeQXo_dKM6syyRLz-gz19JXEABigdwDO4';
 }
