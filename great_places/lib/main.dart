72e5860b77df72f92de0cd74e3059e7cc62a09fa
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-12-21 11:45:26 -0300

    mostrando imagem statica do mapa

--- great_places/lib/main.dart
+++ great_places/lib/main.dart
@@ -5,6 +5,19 @@ import 'package:provider/provider.dart';
 import './screens/places_list_screen.dart';
 import 'provider/great_places.dart';
 
+/*
+Erro no localization, tive que colocar:
+
+location:
+    git:
+      url: https://github.com/781flyingdutchman/flutterlocation.git
+      ref: 'V4'
+      path: packages/location
+
+https://github.com/Lyokone/flutterlocation/issues/843
+https://github.com/Lyokone/flutterlocation/issues/838
+*/
+
 void main() {
   runApp(MyApp());
 }
